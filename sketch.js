adc193a9e68557481a5230ed4b4e473925b26248
ayesha468 <73172478+ayesha468@users.noreply.github.com>
2021-05-02 16:10:06 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,48 @@
+
+const Engine = Matter.Engine;
+const World = Matter.World;
+const Bodies = Matter.Bodies;
+const Body = Matter.Body;
+
+function preload()
+{
+	
+}
+
+function setup() {
+	createCanvas(1200, 300);
+
+
+	engine = Engine.create();
+	world = engine.world;
+
+	box1=new Dustbin(800,275,200,20)
+	box2=new Dustbin(850,270,20,200)
+	ground=new Ground(600,290,1200,20)
+	paperball=new Paper(200,275,25)
+
+
+	Engine.run(engine);
+  
+}
+
+
+function draw() {
+  rectMode(CENTER);
+  background(0);
+  box1.display();
+  box2.display();
+  ground.display();
+  paperball.display();
+  keyPressed();
+  drawSprites();
+ 
+}
+function keyPressed(){
+	if(keyWentDown(UP_ARROW)){
+Matter.Body.applyForce(paperball.body,paperball.body.position,{x:25,y:25})
+	}
+}
+
+
+
